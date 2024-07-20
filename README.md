a6e3120daed604a4dc9d99e1544eb1e212df9f2a
littleblack666333 <124763479+littleblack666333@users.noreply.github.com>
2024-07-20 19:25:49 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,75 @@
+# Verilog Driver Code for LM35A Temperature Sensor
+## Basic Description
+This code is a Verilog driver for the LM35A temperature sensor, used to read the temperature values reported by the 
+sensor using an FPGA. The LM35A is a temperature sensor that operates with the I2C protocol and offers a temperature 
+measurement accuracy of up to 0.125 degrees Celsius. The directly read data is in the form of an 11-bit two's 
+complement. In this driver, to avoid instability caused by excessively high precision, the read temperature precision 
+is set to 0.5 degrees Celsius. This driver includes the conversion of directly read two's complement data into 8421 BCD 
+code and outputs the temperature's sign, hundreds, tens, and units digit data. By default, the driver reads the 
+temperature once per second. This driver module only includes the function to directly read temperature data from the 
+LM35A temperature sensor and does not include the configuration of related registers in the LM35A chip.
+## Driver Module Description
+### Port Information
+This module contains 2 input ports, 7 output ports, and 1 inout port. The port definitions are as follows:
+#### Input Ports:
+* `clk`: 1-bit wide clock signal, default at 50MHz.
+* `rst_n`: 1-bit wide reset signal, active low.
+#### Output Ports:
+* `valid`: 1-bit wide output valid signal, which generates a rising edge after completing temperature reading and 
+  related data processing. This is used to indicate that the subsequent module can receive the temperature data. 
+  The temperature data output will not change between two rising edges of the `valid` signal.
+* `sign`: 1-bit wide signal representing the sign of the temperature. If 0, the temperature is positive; if 1, 
+  the temperature is negative.
+* `fractional`: 4-bit wide signal representing the decimal part of the temperature data (in 8421 BCD code), which is 
+  the first digit after the decimal point. Since the read precision of the driver module is 0.5 degrees Celsius, the 
+  data output by this port can only be 0 or 5.
+* `ones`: 4-bit wide signal representing the units digit of the temperature data (in 8421 BCD code).
+* `tens`: 4-bit wide signal representing the tens digit of the temperature data (in 8421 BCD code).
+* `hundreds`: 4-bit wide signal representing the hundreds digit of the temperature data (in 8421 BCD code). Generally, 
+  the device works below 100 degrees Celsius, so this port can be left floating.
+* `scl`: 1-bit wide I2C protocol `SCL` signal, providing the clock signal.
+#### Inout Port:
+* `sda`: 1-bit wide I2C protocol `SDA` signal, used for data input and output.
+### Configurable Parameters
+* `DEVICE_ADDR`: The address of the LM35A device. The first 4 bits (1001) cannot be modified. The middle 3 bits are the 
+  LM35A's `A2`, `A1`, `A0` address bits, which should be modified according to the actual situation. The default in 
+  the driver is `000`. The last bit represents the read/write signal, with `1` indicating data read and `0` indicating 
+  data write. Since this driver only includes the function of reading temperature data, the last bit should remain `1`.
+* `READ_TIME`: The number of `clk` clock signal cycles included in one temperature reading, used to control the reading 
+  frequency. When the `clk` signal is 50MHz, the default reading cycle is 1s.
+* `STEP_TIME`: The number of `clk` signal cycles included in one period of the `scl` signal during the reading process, 
+  used to control the frequency of the `scl` signal. This frequency should be set according to the actual device 
+  situation and should not be too fast. Additionally, it is recommended to set the last 2 bits of this constant 
+  (in binary representation) to `0`. When the `clk` signal frequency is 50MHz, the default period of the `scl` signal 
+  is 1us.
+------------------------------------------------------------------------------------------------------------------------
+## 基本描述
+本代码是LM35A温度传感器的Verilog驱动程序，用于利用FPGA读取该传感器报告的温度值。LM35A是一款I2C协议的温度传感器，温度测量精度可达到
+0.125摄氏度，直接读取获得的数据是11位补码的形式。在本驱动程序，为了避免精度过高带来的不稳定，读取的温度精度仅为0.5摄氏度。本驱动包含
+将直接读取到的补码转换为8421BCD码的部分，将原始数据转换为温度的符号、百位、十位、个位数据输出。默认情况下，驱动每秒读取一次温度。本驱
+动模块仅包含直接读取LM35A温度传感器的温度数据的功能，不含配置LM35A芯片中的相关寄存器的功能。
+## 驱动模块描述
+### 端口信息
+本模块包含2个输入端口，7个输出端口，1个双向端口，端口定义如下：
+#### 输入端口：
+* `clk`: 位宽为1位，时钟信号，默认为50MHz。
+* `rst_n`: 位宽为1位，复位信号，低有效。
+#### 输出端口：
+* `valid`: 位宽为1位，输出有效信号，当完成温度的读取和相关数据处理后会产生一个上升沿，用于指示后续模块接收温度数据，在`valid`信号的
+  两次上升沿之间输出的温度数据不会改变。
+* `sign`: 位宽为1位，表示温度的符号，若为0，则温度值为正，若为1，则温度值为负。
+* `fractional`: 位宽为4位，表示温度数据的小数部分（8421BCD码），即小数点后第一位的数据。由于驱动模块的读取精度为0.5℃，因此该端口
+  输出的数据仅可能为0或5。
+* `ones`: 位宽为4位，表示温度数据的个位数字（8421BCD码）。
+* `tens`: 位宽为4位，表示温度数据的十位数字（8421BCD码）。
+* `hundreds`: 位宽为4位，表示温度数据的百位数字（8421BCD码）。一般情况下，器件工作的温度低于100℃，因此可以将该端口悬空。
+* `scl`: 位宽为1位，I2C协议的`SCL`信号，提供时钟信号。
+#### 双向端口：
+* `sda`: 位宽位1位，I2C协议的`SDA`信号，用于数据的输入和输出。
+### 可修改参数信息
+* `DEVICE_ADDR`: LM35A器件的地址。前4位1001不可修改。中间3位分别为LM35A的`A2`、`A1`、`A0`地址位，应根据实际情况修改，驱动程序中
+  默认为`000`。最后一位表示读/写信号，`1`表示读取数据，`0`表示写入数据，由于本驱动仅包含读取温度数据的功能，因此应保持最后一位
+  为`1`。
+* `READ_TIME`: 读取一次温度包含的`clk`时钟信号的周期个数，用于控制读取温度的频率。在`clk`信号为50MHz的情况下，默认的读取周期为1s。
+* `STEP_TIME`: 在读取过程中`scl`信号一个周期包含的clk信号的周期个数，用于控制`scl`信号的频率。该频率应根据实际的器件情况设定，不应
+  过快。此外，建议将该常数（以二进制表示）的最后2位数据设定为`0`。在`clk`信号频率为50MHz的情况下，`scl`信号的周期默认为1us。
---
+++ lm75a_driver.v
@@ -0,0 +1,321 @@
+// driver for LM75A temperature sensor
+// using the I2C communication protocol, the temperature accuracy is 0.5℃
+// the raw temperature data has been converted to 8421 BCD code
+
+module lm75a_driver(
+    clk,
+    rst_n,
+    valid,
+    sign,
+    fractional,
+    ones,
+    tens,
+    hundreds,
+    scl,
+    sda
+);
+
+    input clk;  // system clock
+    input rst_n;  // reset signal, active low
+    output valid;  // temperature data validation signal
+    output sign;  // sign of temperature, 0: passive, 1: negative
+    output [3:0] fractional;  // *0.1, 8421 BCD code
+    output [3:0] ones;  // *1, 8421 BCD code
+    output [3:0] tens;  // *10, 8421 BCD code
+    output [3:0] hundreds;  // *100, 8421 BCD code
+    output scl;  // scl port of i2c protocol
+    inout sda;  // sda port of i2c protocol
+
+    wire clk;
+    wire rst_n;
+    reg valid;
+    reg sign;
+    reg [3:0] fractional;
+    reg [3:0] ones;
+    reg [3:0] tens;
+    reg [3:0] hundreds;
+    reg scl;
+    wire sda;
+
+    // self-defined parameters
+    parameter DEVICE_ADDR = 8'b1001_000_1;  // address of lm75a device, A2 A1 AO are self-defined bits
+    parameter READ_TIME = 32'd50_000_000;  // time of one read period, for 50MHz clk freq, it is 1s(1Hz)
+    parameter STEP_TIME = 9'd500;  // for 50MHz clk freq, step time is 10us(100KHz), one step for one scl period
+
+    // state index
+    localparam IDLE = 4'd0;
+    localparam START = 4'd1;
+    localparam TRANS_ADDR = 4'd2;
+    localparam READ_MSB = 4'd3;
+    localparam READ_LSB = 4'd4;
+    localparam FINISH = 4'd5;
+
+    reg sda_dir;  // data direction on sda, 1: master -> device, 0: master <- device
+    wire sda_input;  // sda data input
+    reg sda_output;  // sda data output
+    reg [31:0] cnt_read;  // counter of read period
+    reg read_flag;  // trigger signal of a read period
+    reg [8:0] cnt_step;  // counter of step, period of cnt_step is one step time
+    reg en_cnt_step;  // enable signal of cnt_step, only if 1 cnt_step will work
+    reg [3:0] cnt_bit;  // counter of bit in step, using cnt_bit == 4'd8 as end symbol
+    reg en_cnt_bit;  // enable signal of cnt_bit, only if 1 cnt_bit will work
+    reg [3:0] state;  // state mark
+    reg [15:0] buff;  // buffer of data directly read from sda_input
+    reg [7:0] data;  // 8-bit binary temperature data (sign bit not included)
+    reg [7:0] data_temp;  // temporary register used to decode temperature data
+    integer i;  // iteration variable used to decode temperature data
+
+    // instance of inout sda
+    assign sda_input = sda;
+    assign sda = sda_dir ? sda_output : 1'bz;
+
+    // def of cnt_read and read_flag
+    always @(negedge rst_n or posedge clk) begin
+        if (~rst_n) begin
+            cnt_read <= 32'b0;
+            read_flag <= 1'b0;
+        end
+        else if (cnt_read == READ_TIME - 1'b1) begin
+            cnt_read <= 32'b0;
+            read_flag <= 1'b1;
+        end
+        else begin
+            cnt_read <= cnt_read + 1'b1;
+            read_flag <= 1'b0;
+        end
+    end
+
+    // def of cnt_step
+    always @(negedge rst_n or posedge clk) begin
+        if (~rst_n) begin
+            cnt_step <= 9'b0;
+        end
+        else if (en_cnt_step) begin
+            if (cnt_step == STEP_TIME - 1'b1) begin
+                cnt_step <= 9'b0;
+            end
+            else begin
+                cnt_step <= cnt_step + 1'b1;
+            end
+        end
+        else begin  // !en_cnt_step
+            cnt_step <= 9'b0;
+        end
+    end
+    
+    // def of scl
+    always @(negedge rst_n or posedge clk) begin
+        if (~rst_n) begin
+            scl <= 1'b1;
+        end
+        else if (en_cnt_step)  begin
+            if (cnt_step == STEP_TIME[8:1] - 1'b1) begin
+                scl <= 1'b1;
+            end
+            else if (cnt_step == STEP_TIME - 1'b1) begin
+                scl <= 1'b0;
+            end
+            else begin
+                scl <= scl;
+            end
+        end
+        else begin  // !en_cnt_step
+            scl <= 1'b1;
+        end
+    end
+
+    // def of cnt_bit
+    always @(negedge rst_n or posedge clk) begin
+        if (~rst_n) begin
+            cnt_bit <= 4'b0;
+        end
+        else if (en_cnt_bit) begin
+            if (cnt_step == STEP_TIME - 1'b1) begin
+                if (cnt_bit == 4'd8) begin
+                    cnt_bit <= 4'b0;
+                end
+                else begin
+                    cnt_bit <= cnt_bit + 1'b1;
+                end
+            end
+        end
+        else begin  // !en_cnt_bit
+            cnt_bit <= 4'b0;
+        end
+    end
+
+    // i2c main module
+    always @(negedge rst_n or posedge clk) begin
+        if (~rst_n) begin
+            valid <= 1'b0;
+            sda_dir <= 1'b1;
+            sda_output <= 1'b1;
+            en_cnt_step <= 1'b0;
+            en_cnt_bit <= 1'b0;
+            state <= IDLE;
+            buff <= 16'b0;
+            data <= 8'b0;
+            sign <= 1'b0;
+        end
+        else begin
+            case (state)
+                IDLE: begin  // IDLE state, reset related flags
+                    valid <= 1'b0;
+                    sda_dir <= 1'b1;
+                    sda_output <= 1'b1;
+                    en_cnt_step <= 1'b0;
+                    en_cnt_bit <= 1'b0;
+
+                    if (read_flag) begin  // wait for trigger signal
+                        en_cnt_step <= 1'b1;
+                        state <= START;
+                    end
+                end
+
+                START: begin  // transfer start signal
+                    if (cnt_step == STEP_TIME[8:1] - 1'b1) begin
+                        sda_dir <= 1'b1;
+                        sda_output <= 1'b0;
+                    end
+                    else if (cnt_step == STEP_TIME - 1'b1) begin
+                        en_cnt_bit <= 1'b1;
+                        state <= TRANS_ADDR;
+                    end
+                end
+
+                TRANS_ADDR: begin  // transfer device address and receive device acknowledge
+                    if (cnt_step == STEP_TIME - 1'b1) begin  // check device acknowledge
+                        if (cnt_bit == 4'd8) begin
+                            if (sda_input == 1'b0) begin  // device acknowledge
+                                state <= READ_MSB;
+                            end
+                            else begin  // device not acknowledge
+                                state <= IDLE;
+                            end
+                        end
+                    end
+
+                    if (cnt_bit < 4'd8) begin  // transfer device address
+                        if (cnt_step == STEP_TIME[8:2] - 1'b1) begin
+                            sda_dir <= 1'b1;
+                            sda_output <= DEVICE_ADDR[4'd7 - cnt_bit];
+                        end
+                    end
+                    else begin  // receive device acknowledge
+                        if (cnt_step == STEP_TIME[8:2] - 1'b1) begin
+                            sda_dir <= 1'b0;
+                        end
+                    end
+                end
+
+                READ_MSB: begin  // read MSB of temperature data, and transfer master acknowledge
+                    if (cnt_step == STEP_TIME - 1'b1) begin
+                        if (cnt_bit == 4'd8) begin
+                            sda_dir <= 1'b0;
+                            state <= READ_LSB;
+                        end
+                    end
+
+                    if (cnt_bit < 4'd8) begin  // receive temperature data
+                        if (cnt_step == STEP_TIME[8:1] + STEP_TIME[8:2] - 1'b1) begin
+                            buff <= {buff[14:0], sda_input};
+                        end
+                    end
+                    else begin  // master acknowledge
+                        if (cnt_step == STEP_TIME[8:2] - 1'b1) begin
+                            sda_dir <= 1'b1;
+                            sda_output <= 1'b0;
+                        end
+                    end
+                end
+
+                READ_LSB: begin  // read LSB of temperature data, but master not acknowledge at the end
+                    if (cnt_step == STEP_TIME - 1'b1) begin
+                        if (cnt_bit == 4'd8) begin
+                            state <= FINISH;
+                        end
+                    end
+
+                    if (cnt_bit < 4'd8) begin  // receive temperature data
+                        if (cnt_step == STEP_TIME[8:1] + STEP_TIME[8:2] - 1'b1) begin
+                            buff <= {buff[14:0], sda_input};
+                        end
+                    end
+                    else begin  // master not acknowledge
+                        if (cnt_step == STEP_TIME[8:2] - 1'b1) begin
+                            sda_dir <= 1'b1;
+                            sda_output <= 1'b1;
+                        end
+                    end
+                end
+
+                FINISH: begin  // transfer end signal and refresh temperature data
+                    if (cnt_step == STEP_TIME[8:2] - 1'b1) begin  // transfer end signal
+                        sda_dir <= 1'b1;
+                        sda_output <= 1'b0;
+                    end
+                    else if (cnt_step == STEP_TIME[8:1] + STEP_TIME[8:2] - 1'b1) begin
+                        sda_dir <= 1'b1;
+                        sda_output <= 1'b1;
+                    end
+                    else if (cnt_step == STEP_TIME - 2'd2) begin  // extract data from buffer
+                        if (buff[15] == 1'b0) begin  // passive data
+                            data <= buff[14:7];
+                        end
+                        else begin  // negative data
+                            data <= ~buff[14:7] + 1'b1;
+                        end
+                        sign <= buff[15];
+                    end
+                    else if (cnt_step == STEP_TIME - 1'b1) begin  // reset flags
+                        valid <= 1'b1;
+                        en_cnt_step <= 1'b0;
+                        state <= IDLE;
+                    end
+                end
+
+                default: begin
+                    valid <= 1'b0;
+                    sda_dir <= 1'b1;
+                    sda_output <= 1'b1;
+                    en_cnt_step <= 1'b0;
+                    en_cnt_bit <= 1'b0;
+                    state <= IDLE;
+                    buff <= 16'b0;
+                    data <= 8'b0;
+                    sign <= 1'b0;
+                end
+            endcase
+        end
+    end
+
+    // data decoding
+    always @(data[0]) begin  // fractional part
+        case (data[0])
+            1'b1: fractional = 4'd5;
+            default: fractional = 4'd0;
+        endcase
+    end
+
+    always @(data[7:1]) begin  // integer part
+        ones = 4'd0;
+        tens = 4'd0;
+        hundreds = 4'd0;
+        data_temp = {1'b0, data[7:1]};
+        for (i = 0; i < 8; i = i + 1'b1) begin
+            if (ones > 4'd5) begin
+                ones = ones + 4'd3;
+            end
+            if (tens > 4'd5) begin
+                tens = tens + 4'd3;
+            end
+            if (hundreds > 4'd5) begin
+                hundreds = hundreds + 4'd3;
+            end
+            hundreds = {hundreds[2:0], tens[3]};
+            tens = {tens[2:0], ones[3]};
+            ones = {ones[2:0], data_temp[7 - i]};
+        end
+    end
+
+endmodule
